a06885fec0dddeecc012a1e360a61a3e4a4118cd
asepburhan <91001819+asepburhan@users.noreply.github.com>
2021-11-12 12:55:36 +0700

    Add files via upload

---
+++ aute_footer.php
@@ -0,0 +1,10 @@
+<!-- Bootstrap core JavaScript-->
+<script src="assets/css/vendor/jquery/jquery.min.js"></script>
+    <script src="assets/css/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="assets/css/vendor/jquery-easing/jquery.easing.min.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="assets/css/js/sb-admin-2.min.js"></script>
+</body>
+
+</html>
